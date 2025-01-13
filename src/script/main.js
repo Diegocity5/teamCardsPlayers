46312a75854c47d1d66a67e2f80a7f4284b2b21f
Diegocity5 <icotecc@gmail.com>
2025-01-12 20:19:06 -0500

    4: Selecionando el elemento selector de jugadores

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,7 @@
+//Selecionando variables y elementos del DOM para interactuar.
 const teamName = document.getElementById('team');
 const typeOfSport = document.getElementById('sport');
 const worldCupYear = document.getElementById('year');
 const headCoach = document.getElementById('head-coach');
 const playerCards = document.getElementById('player-cards');
+const playersDropdownList = document.getElementById('players');
