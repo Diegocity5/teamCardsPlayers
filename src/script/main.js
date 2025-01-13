b82c6c3ff6ba4b8acaa77bc6b8396ac600bbc00f
Diegocity5 <icotecc@gmail.com>
2025-01-12 21:58:25 -0500

    11: Agregue mas jugadores al la estructura

--- src/script/main.js
+++ src/script/main.js
@@ -21,9 +21,137 @@ const myFavoriteFootballTeam = {
             name: "Sergio Almiron",
             position: "forward",
             number: 1,
-            isCapitan: false,
+            isCaptain: false,
+            nickname: null
+        },
+        {
+            name: "Sergio Batista",
+            position: "midfielder",
+            number: 2,
+            isCaptain: false,
+            nickname: null
+        },
+        {
+            name: "Ricardo Bochini",
+            position: "midfielder",
+            number: 3,
+            isCaptain: false,
+            nickname: "El Bocha"
+        },
+        {
+            name: "Claudio Borghi",
+            position: "midfielder",
+            number: 4,
+            nickname: "Bichi"
+        },
+        {
+            name: "José Luis Brown",
+            position: "defender",
+            number: 5,
+            nickname: "tata"
+        },
+        {
+            name: "Daniel Passarella",
+            position: "defender",
+            number: 6,
+            nickname: "El Gran Capitán"
+        },
+        {
+            name: "Jorge Burruchaga",
+            position: "forward",
+            number: 7,
+            nickname: "Burru"
+        },
+        {
+            name: "Néstor Clausen",
+            position: "defender",
+            number: 8,
+            nickname: null
+        },
+        {
+            name: "José Luis Cuciuffo",
+            position: "defender",
+            number: 9,
+            nickname: "El Chuchu"
+        },
+        {
+            name: "Diego Maradona",
+            position: "midfielder",
+            number: 10,
+            nickname: "El Pibe de Oro"
+        },
+        {
+            name: "Jorge Valdano",
+            position: "forward",
+            number: 11,
+            nickname: "The Philosopher of Football"
+        },
+        {
+            name: "Héctor Enrique",
+            position: "midfielder",
+            number: 12,
+            nickname: null
+
+        },
+        {
+            name: "Oscar Garré",
+            position: "defender",
+            number: 13,
+            nickname: null
+        },
+        {
+            name: "Ricardo Giusti",
+            position: "midfielder",
+            number: 14,
+            nickname: null
+        },
+        {
+            name: "Luis Islas",
+            position: "goalkeeper",
+            number: 15,
+            nickname: "El loco"
+        },
+        {
+            name: "Julio Olarticoechea",
+            position: "defender",
+            number: 16,
+            nickname: null
+        },
+        {
+            name: "Pedro Pasculli",
+            position: "forward",
+            number: 17,
+            nickname: null
+        },
+        {
+            name: "Nery Pumpido",
+            position: "goalkeeper",
+            number: 18,
+            nickname: null
+        },
+        {
+            name: "Oscar Ruggeri",
+            position: "defender",
+            number: 19,
+            nickname: "El cabezón"
+        },
+        {
+            name: "Carlos Tapia",
+            position: "midfielder",
+            number: 20,
+            nickname: null
+        },
+        {
+            name: "Marcelo Trobbiani",
+            position: "midfielder",
+            number: 21,
+            nickname: "Calesita"
+        },
+        {
+            name: "Hector Zelada",
+            position: "goalkeeper",
+            number: 22,
             nickname: null
         }
-        
     ]
 };
