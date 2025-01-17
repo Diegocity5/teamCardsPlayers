f26ac4f8723f5bb7bdd48dddf6e58b6f653e22d9
Diegocity5 <icotecc@gmail.com>
2025-01-17 00:09:58 -0500

    26: Agregando un <h2> etiqueta HTML y interpolo la variable name

--- src/script/main.js
+++ src/script/main.js
@@ -194,6 +194,7 @@ const setPlayerCards = ( arr=players )=>{
     playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname})=>{
         return `
         <div class="player-card">
+            <h2>${name}</h2>
         </div>`;
     });
 };
